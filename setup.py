a9004b542b6f77368b4c467be99ca6a6f5885e6d
Yuji Sakurai <yuji_sakurai@r.recruit.co.jp>
2018-09-30 00:12:24 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 from setuptools import setup
 
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
 requires = [
     "pyhcl>=0.3.00",
 ]
@@ -7,8 +13,10 @@ requires = [
 
 setup(
     name='tf_cop',
-    version='0.0.1',
+    version='0.0.2',
     description='auto terraform review',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     url='https://github.com/ys-tydy/tf_cop',
     author='ys-tydy',
     author_email='',
@@ -23,3 +31,7 @@ setup(
         'Programming Language :: Python :: 3.6',
     ],
 )
+
+# python setup.py sdist
+# twine check dist/*
+# twine upload dist/*
